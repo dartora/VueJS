f6b5d8ce6498b1cb5ad2b1252fecd3795354321d
Rodolfo <rodolfo.dartora.da.silva@gmail.com>
2022-11-10 15:49:20 -0300

    agoraQuase

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
-import Vue from "vue";
+// import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createApp } from "vue";
 
 // createApp(App).mount('#app')
 
 /* eslint-disable no-new */
-new Vue({
-  el: "#app",
+// new Vue({
+//   el: "#app",
+//   router,
+//   components: { App },
+//   template: "<App/>",
+// });
+
+createApp({
   router,
   components: { App },
   template: "<App/>",
-});
+}).mount("#app");
